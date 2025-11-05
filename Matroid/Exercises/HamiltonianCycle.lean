317502e8a56126f956ca58d68331200c4c1a732b
Richard Zhang <73731354pi@gmail.com>
2025-11-04 20:35:15 -0500

    playing around with some defs for WList

--- Matroid/Exercises/HamiltonianCycle.lean
+++ Matroid/Exercises/HamiltonianCycle.lean
@@ -71,6 +71,269 @@ lemma maximalFor_is_upper_bound
 
 end NonGraphThings
 
+namespace WList
+
+variable {α β : Type*} {x y z u v : α} {e f : β} {w : WList α β}
+
+-- this is probably a bad idea, but let's test it first and see *how* bad it can get
+def getEdge? : WList α β → ℕ → Option β
+  | nil _, _ => none
+  | cons _ e _, 0 => some e
+  | cons _ _ w, n + 1 => w.getEdge? n
+
+@[simp]
+lemma getEdge?_nil (x : α) (n : ℕ) : (nil x (β := β)).getEdge? n = none := rfl
+
+@[simp]
+lemma getEdge?_cons_zero (x e) (w : WList α β) : (cons x e w).getEdge? 0 = some e := rfl
+
+@[simp]
+lemma getEdge?_cons_succ (x e) (w : WList α β) (n : ℕ) :
+    (cons x e w).getEdge? (n + 1) = w.getEdge? n := rfl
+
+@[simp]
+lemma getEdge?_eq_none_of_length_le {n} (hn : w.length ≤ n) : w.getEdge? n = none := by
+  induction w generalizing n with
+  | nil _ => simp
+  | cons _ _ _ => induction n with simp_all
+
+@[simp]
+lemma getEdge?_isNone_of_length_le {n} (hn : w.length ≤ n) : (w.getEdge? n).isNone := by
+  simp [getEdge?_eq_none_of_length_le hn]
+
+@[simp]
+lemma lt_length_of_getEdge?_isSome (w : WList α β) (n : ℕ) (h : (w.getEdge? n).isSome) :
+    n < w.length := by
+  by_contra! hyp
+  rw [getEdge?_eq_none_of_length_le hyp] at h
+  exact (Bool.eq_not_self none.isSome).mp h
+
+lemma exists_of_getEdge?_lt_length (w : WList α β) {n} (hn : n < w.length) :
+    ∃ f, w.getEdge? n = some f ∧ f ∈ w.edge := by
+  induction n generalizing w with
+  | zero => cases w <;> simp_all
+  | succ n IH =>
+    cases w with simp_all
+    | cons x e w =>
+      obtain ⟨e, he⟩ := IH _ hn
+      use e; tauto
+
+lemma exists_mem_of_getEdge?_isSome (w : WList α β) (n : ℕ) (h : (w.getEdge? n).isSome) :
+    ∃ e, w.getEdge? n = some e ∧ e ∈ w.edge := by
+  apply exists_of_getEdge?_lt_length
+  apply lt_length_of_getEdge?_isSome; assumption
+
+lemma getEdge?_eq_getElem?_edge (w : WList α β) (n : ℕ) : w.getEdge? n = w.edge[n]? := by
+  induction n generalizing w with
+  | zero => cases w <;> simp_all
+  | succ n IH => cases w with simp_all
+
+lemma mem_of_getEdge? (w : WList α β) (n : ℕ) {f} (h : w.getEdge? n = some f) :
+    f ∈ w.edge := by
+  rw [getEdge?_eq_getElem?_edge] at h
+  exact List.mem_of_getElem? h
+
+variable [DecidableEq β]
+
+-- returns the index of the edge, equal to `w.length` if the edge doesn't appear
+def idxOfEdge : WList α β → β → ℕ
+  | nil _, _ => 0
+  | cons _ e' w, e =>
+    if e = e' then 0 else w.idxOfEdge e + 1
+
+@[simp]
+lemma idxOfEdge_nil (u : α) (e : β) : (nil u (β := β)).idxOfEdge e = 0 := rfl
+
+@[simp]
+lemma idxOfEdge_cons (u e) (w : WList α β) :
+    (cons u e w).idxOfEdge f = if f = e then 0 else w.idxOfEdge f + 1 := rfl
+
+@[simp]
+lemma idxOfEdge_cons_self (u e) (w : WList α β) :
+    (cons u e w).idxOfEdge e = 0 := by simp [idxOfEdge_cons]
+
+@[simp]
+lemma idxOfEdge_cons_ne (hne : f ≠ e)  :
+    (cons u e w).idxOfEdge f = w.idxOfEdge f + 1 := by
+  simp [idxOfEdge_cons, hne]
+
+lemma idxOfEdge_notMem (hf : f ∉ w.edge) : w.idxOfEdge f = w.length := by
+  induction w with
+  | nil => simp
+  | cons u e w IH =>
+    simp at hf
+    rw [←ne_eq] at hf
+    specialize IH hf.2
+    rw [idxOfEdge_cons_ne hf.1]; try assumption
+    simp [IH]
+
+lemma eq_or_ne_mem_of_mem_cons_edge (h : f ∈ (cons u e w).edge) :
+    f = e ∨ (f ≠ e ∧ f ∈ w.edge) := by
+  obtain rfl | hne := Decidable.eq_or_ne f e ; simp
+  right; refine ⟨hne, ?_⟩
+  simp_all
+
+lemma idxOfEdge_mem_le (hf : f ∈ w.edge) : w.idxOfEdge f < w.length := by
+  induction w with
+  | nil => simp_all
+  | cons u e w IH =>
+    obtain rfl | ⟨hne, hfw⟩ := eq_or_ne_mem_of_mem_cons_edge hf; simp; clear hf
+    specialize IH hfw
+    rw [idxOfEdge_cons_ne ‹_›, cons_length]
+    linarith
+
+lemma idxOfEdge_eq_idxOf_edge (w : WList α β) (f : β) : w.idxOfEdge f = w.edge.idxOf f := by
+  induction w with
+  | nil => simp
+  | cons u e w IH =>
+    obtain rfl | hne := Decidable.eq_or_ne f e; simp
+    simp [idxOfEdge_cons_ne ‹_›, IH, List.idxOf_cons_ne _ hne.symm]
+
+lemma getEdge?_idxOfEdge (w : WList α β) (hfw : f ∈ w.edge) :
+    w.getEdge? (w.idxOfEdge f) = some f := by
+  rwa [getEdge?_eq_getElem?_edge, idxOfEdge_eq_idxOf_edge, List.getElem?_idxOf]
+
+
+lemma existsUnique_of_getEdge?_lt_length' (hw : w.edge.Nodup) {n} (hn : n < w.length) :
+    ∃! f, w.getEdge? n = some f ∧ w.idxOfEdge f = n := by
+  induction n generalizing w with
+  | zero =>
+    cases w with simp_all
+    | cons x e w => use e; simp
+  | succ n IH =>
+    cases w with simp_all
+    | cons x e w =>
+      obtain ⟨f, hf, hf_unique⟩ := IH hw.2 hn
+      refine ⟨f, ?_, by simp_all⟩
+      simp
+      refine ⟨hf.1, ?_⟩
+      have hne : f ≠ e := by
+        by_contra! rfl
+        have := w.mem_of_getEdge? _ hf.1
+        tauto
+      rw [if_neg (by rwa [←ne_eq])]
+      linarith
+
+lemma idxOfEdge_getEdge? (hw : w.edge.Nodup) {n} (hn : n < w.length) :
+    (w.getEdge? n).map w.idxOfEdge = some n := by
+  have ⟨f', hf', hf'_unique⟩ := w.existsUnique_of_getEdge?_lt_length' hw hn
+  simp_all
+
+
+-- given an edge f, we return the left and right vertices of the first instance of
+-- of f in w, defaulting to the last vertex if f is not in w
+def adjVertices (w : WList α β) (f : β) : α × α :=
+  ⟨w.get (w.idxOfEdge f), w.get (w.idxOfEdge f + 1)⟩
+
+lemma adjVertices_eq_last_of_notMem (h : f ∉ w.edge) :
+    w.adjVertices f = ⟨w.last, w.last⟩ := by
+  simp [adjVertices, idxOfEdge_notMem h, get_of_length_le]
+
+-- if it is a member, then it forms a DInc triplet
+lemma dInc_adjVertices_of_mem (h : f ∈ w.edge) :
+    let uu := w.adjVertices f;
+    w.DInc f uu.1 uu.2 := by
+  simp only [adjVertices]
+  generalize n_def : w.idxOfEdge f = n
+  induction w generalizing n with
+  | nil => simp_all
+  | cons x e w IH =>
+    simp_all
+    obtain rfl | hne := Decidable.eq_or_ne f e <;>
+      (simp_all; subst n_def)
+    · left; simp
+    · right; simpa
+
+-- in a WList with no repeated edges, each edge is part of exactly one DInc triplet
+lemma dInc_iff_eq_of_dInc_of_edge_nodup (hw : w.edge.Nodup) (he : w.DInc e u v) :
+    w.DInc e x y ↔ x = u ∧ y = v := by
+  refine ⟨fun h ↦ ?_, by rintro ⟨rfl, rfl⟩; assumption⟩
+  induction w with
+  | nil => simp_all
+  | cons z f w IH =>
+    simp at hw h he
+    obtain ⟨rfl, rfl, rfl⟩ | h := h
+    · obtain ⟨rfl, he, rfl⟩ | he := he; try tauto
+      exfalso; apply hw.1; apply he.edge_mem
+    obtain ⟨rfl, rfl, rfl⟩ | he := he
+    · exfalso; apply hw.1; apply h.edge_mem
+    apply IH <;> first | assumption | tauto
+
+lemma existsUnique_dInc_of_mem_edge (hw : w.edge.Nodup) (he : e ∈ w.edge) :
+    ∃! p : α × α, w.DInc e p.1 p.2 := by
+  have ⟨x, y, hxy⟩ := exists_dInc_of_mem_edge he
+  refine ⟨⟨x,y⟩, hxy, ?_⟩
+  simp [dInc_iff_eq_of_dInc_of_edge_nodup hw hxy]
+
+lemma dInc_iff_eq_of_dInc_of_vertex_nodup_left (hw : w.vertex.Nodup) (hu : w.DInc e u v) :
+    w.DInc f u y ↔ f = e ∧ y = v := by
+  refine ⟨fun h ↦ ?_, by rintro ⟨rfl, rfl⟩; assumption⟩
+  induction w with
+  | nil _ => simp_all
+  | cons u' f' w IH =>
+    simp_all
+    obtain ⟨rfl, rfl, rfl⟩ | h := h
+    · obtain ⟨hu, rfl, rfl⟩ | hu := hu; try tauto
+      exfalso; apply hw.1; apply hu.left_mem
+    obtain ⟨rfl, rfl, rfl⟩ | hu := hu
+    · exfalso; apply hw.1; apply h.left_mem
+    apply IH <;> assumption
+
+lemma dInc_iff_eq_of_dInc_of_vertex_nodup_right (hw : w.vertex.Nodup) (hv : w.DInc e u v) :
+    w.DInc f x v ↔ f = e ∧ x = u := by
+  generalize hw_def' : w.reverse = w'
+  have hw' : w'.vertex.Nodup := by rwa [← hw_def', reverse_vertex, List.nodup_reverse]
+  have hv' : w'.DInc e v u := by simpa [← hw_def']
+  have := dInc_iff_eq_of_dInc_of_vertex_nodup_left (f := f) (v := u) (y := x) hw' hv'
+  rwa [← hw_def', dInc_reverse_iff] at this
+
+-- -- In a WList with no repeated vertices, each non-last vertex is part of exactly one DInc triplet
+-- -- as the left vertex.
+-- --
+-- -- These two proofs are kinda awful, lol, surely there's a way to do this that's not just puking
+-- -- commands at the screen until you get "goals accomplished".
+-- -- In particular, this proof and the previous proof are *so* structurally similar,
+-- -- but my brain is too off at the moment to come up with a coherent way to combine them.
+-- lemma existsUnique_dInc_of_mem_left (hw : w.vertex.Nodup) (hxw : x ∈ w) (hx : x ≠ w.last) :
+--     ∃! p : β × α, w.DInc p.1 x p.2 := by
+--   induction w with
+--   | nil => simp_all
+--   | cons u e w IH =>
+--     have hne : x ≠ w.last := by simpa
+--     simp at hw
+--     obtain (rfl|hxw) := by simpa using hxw
+--     · refine ⟨⟨e, w.first⟩, ?_, ?_⟩ <;> simp_all
+--       intros f y hyp
+--       obtain (hyp|hyp) := hyp
+--       · tauto
+--       exfalso
+--       apply hw.1
+--       apply hyp.left_mem
+--     · obtain ⟨p, hp, hp_unique⟩ := IH hw.2 hxw hne
+--       refine ⟨p, ?_, ?_⟩
+--       · simp only; apply DInc.cons; assumption
+--       simp only at *
+--       intros q hq
+--       rw [dInc_cons_iff] at hq
+--       obtain (hq|hq) := hq
+--       · exfalso; apply hw.1; rwa [hq.1]
+--       apply hp_unique; assumption
+
+-- lemma existsUnique_dInc_of_mem_right (hw : w.vertex.Nodup) (hxw : x ∈ w) (hx : x ≠ w.first) :
+--     ∃! p : β × α, w.DInc p.1 p.2 x := by
+--   set w' := w.reverse
+--   have hw'_vertex : w'.vertex = w.vertex.reverse := reverse_vertex
+--   have hx' : x ≠ w'.last := by
+--     rwa [reverse_last]
+--   have hw'_nodup : w'.vertex.Nodup := by
+--     rwa [hw'_vertex, List.nodup_reverse]
+--   have hxw' : x ∈ w' := by rwa [mem_reverse]
+--   have answer := existsUnique_dInc_of_mem_left hw'_nodup hxw' hx'
+--   simp [w'] at answer
+--   assumption
+
+end WList
+
 namespace Graph
 
 variable {α β : Type*} [CompleteLattice α] {x y z u v : α} {e f : β} {G H : Graph α β}
@@ -114,6 +377,9 @@ lemma degreeSet_finite_of_finite {G : Graph α β} (hFinite : G.Finite) :
   simp [degreeSet]
   refine Set.Finite.image ?_ ?_
   exact vertexSet_finite
+
+lemma degreeSet_finite (G : Graph α β) [G.Finite] : G.degreeSet.Finite :=
+  degreeSet_finite_of_finite ‹_›
 
 lemma degreeSet_nonempty {G : Graph α β} (hNeBot : G ≠ ⊥) : G.degreeSet.Nonempty := by
   simpa [degreeSet]
